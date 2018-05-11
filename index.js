8f5980a74fb566c675a6f303b132b14583ef5107
rsantosdev <rsantos@braziliandevs.com>
2018-05-10 22:21:03 -0300

    Machine Info

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 const
-    http = require("http");
+    http = require("http"),
+    os = require("os");
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! Machine");
+    response.end(`Hello World! Machine: ${os.hostname()}, Type: ${os.type()}`);
 });
 
 const port = process.env.PORT || 1337;
